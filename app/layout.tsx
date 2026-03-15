768dd65125bbfea064747db1b674ab80627e0e7d
GEN2 <qwennnzim@gmail.com>
2026-03-16 05:16:37 +0700

    feat: Add Google site verification meta tag
    Includes the necessary meta tag for Google site verification, enabling integration with Google search console for site ownership confirmation.

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="U1n-y8RlFf7-B3gJQgjQpM1v4HOPrBhhoXKxGjI86pA" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
